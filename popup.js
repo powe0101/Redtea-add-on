e9e87703eca0d69b3c9dd743dcab33e217b3e9eb
powe0101 <powe0101@naver.com>
2017-05-28 22:16:57 +0900

    하이라이트 기능 추가 완료

--- popup.js
+++ popup.js
@@ -6,9 +6,9 @@
 main();
 
 /*
-  searchForm : 검색창의 폼 섹션 요소 (인덱스 연산)[0]~
-  myNickName : @r_reporter : 닉네임 input
-  mainLeft : 로그인 폼 요소
+  searchForm : @d2 - 검색창의 폼 섹션 요소
+  myNickName : $r_reporter - 닉네임 input
+  mainLeft : @logged - 로그인 폼 요소
 */
 function main()
 {
@@ -20,12 +20,34 @@ function main()
     AddSearchByMyIdButton(searchForm,myNickName);
 
   if(myNickName)
+  {
     PrintMyNickNameAtMain(mainLeft,myNickName);
+    highlightMyNickName(myNickName);
+  }
+}
+
+/*
+  nameList : @tl-name - 타임라인형 게시판 닉네임 태그
+  x : #span - 닉네임 태그의 첫번째 자식 태그, innerText == 작성자 닉네임
+*/
+function highlightMyNickName(_nickName)
+{
+  var nameList = document.getElementsByClassName("tl-name");
+
+  for(var i = 0; i < nameList.length; ++i)
+  {
+    var x = nameList[i].children[0];
+
+    if(x.innerText == _nickName)
+      x.setAttribute("style","cursor:pointer;background-color:#A3CCA3;");
+  }
 }
 
 /*
   각 게시판 별 검색창 요소 부분 추가
   sn : 게시물 기준 닉네임 대상 검색
+  boardName : $r_board_id - 현재 엑티브된 게시판 정보
+  x : @searchByNickName - 닉네임으로 검색 할 수 있는 버튼 추가
 */
 function AddSearchByMyIdButton(_searchForm,_nickName)
 {
@@ -35,6 +57,7 @@ function AddSearchByMyIdButton(_searchForm,_nickName)
   sn.setAttribute("type","hidden");
   sn.setAttribute("name","sn");
   sn.setAttribute("value","on");
+
   document.getElementsByName("search")[0].appendChild(sn);
 
   var x = document.createElement("input");
@@ -48,12 +71,13 @@ function AddSearchByMyIdButton(_searchForm,_nickName)
 
 /*
   mainLeft 지역 닉네임 표시 기능 ( 디버깅용 )
-  실제 코드 상 문제가 있을 경우 이 기능이 동작하지 않음
+  실제 코드 상 문법 단위 문제가 있을 경우 이 기능이 동작하지 않음
 */
 function PrintMyNickNameAtMain(_mainLeft,_nickName)
 {
   var x = document.createElement("div");
   x.className = "myId";
   x.innerText = "내 닉네임 : " + _nickName;
+
   _mainLeft[0].appendChild(x);
 }
